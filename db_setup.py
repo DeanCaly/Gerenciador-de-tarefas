4bf5f89f1c4ce8d4a2a9b8229084c01607a297da
DeanCaly <deancaly@gmail.com>
2025-10-24 22:00:48 -0300

    feat: setup inicial do banco de dados SQLite e tabela 'tasks'

---
+++ db_setup.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+db_file = "tasks.db"
+
+with sqlite3.connect(db_file) as con:
+
+    cursor = con.cursor()
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS tasks("id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT NOT NULL,
+                   descricao TEXT, status BOOLEAN NOT NULL DEFAULT 0") """)
+
+    print(f"Banco de dados {db_file} e tabela task criados com sucesso!")
+
+
+
